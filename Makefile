3ce0eb88eaad32f2e0d9d734bee44306fd186021
Simon Gerber <simon.gerber@vshn.ch>
2023-03-08 14:10:32 +0100

    Make editorconfig linting CI job global

---
+++ Makefile
@@ -0,0 +1,23 @@
+MAKEFLAGS += --warn-undefined-variables
+SHELL := bash
+.SHELLFLAGS := -eu -o pipefail -c
+.DEFAULT_GOAL := all
+.DELETE_ON_ERROR:
+.SUFFIXES:
+
+include Makefile.vars.mk
+
+.PHONY: all
+all: help
+
+.PHONY: help
+help: ## Show this help
+	@grep -E -h '\s##\s' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = "(: ).*?## "}; {gsub(/\\:/,":", $$1)}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}'
+
+.PHONY: lint_adoc
+lint_adoc: ## Lint documentation
+	$(VALE_CMD) $(VALE_ARGS)
+
+.PHONY: docs-serve
+docs-serve: ## Preview the documentation
+	$(ANTORA_PREVIEW_CMD)
---
+++ Makefile.vars.mk
@@ -0,0 +1,24 @@
+COMPONENT_NAME ?= openshift4-service-mesh
+
+git_dir     ?= $(shell git rev-parse --git-common-dir)
+root_volume ?= -v "$${PWD}:/$(COMPONENT_NAME)"
+
+ifneq "$(git_dir)" ".git"
+	antora_git_volume ?= -v "$(git_dir):/preview/antora/.git:ro"
+else
+	antora_git_volume ?= -v "${PWD}/.git:/preview/antora/.git:ro"
+endif
+
+ifneq "$(shell which docker 2>/dev/null)" ""
+	DOCKER_CMD    ?= $(shell which docker)
+	DOCKER_USERNS ?= ""
+else
+	DOCKER_CMD    ?= podman
+	DOCKER_USERNS ?= keep-id
+endif
+DOCKER_ARGS ?= run --rm -u "$$(id -u):$$(id -g)" --userns=$(DOCKER_USERNS) -w /$(COMPONENT_NAME) -e HOME="/$(COMPONENT_NAME)"
+
+VALE_CMD  ?= $(DOCKER_CMD) $(DOCKER_ARGS) $(root_volume) --volume "$${PWD}"/docs/modules:/pages docker.io/vshn/vale:2.1.1
+VALE_ARGS ?= --minAlertLevel=error --config=/pages/ROOT/pages/.vale.ini /pages
+
+ANTORA_PREVIEW_CMD ?= $(DOCKER_CMD) run --rm --publish 35729:35729 --publish 2020:2020 $(antora_git_volume) --volume "${PWD}/docs":/preview/antora/docs docker.io/vshn/antora-preview:3.0.1.1 --style=syn --antora=docs
